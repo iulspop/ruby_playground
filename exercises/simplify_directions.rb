f1811a48673a76720f1bb7d980520f3c4d6ab1ec
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-08-23 09:50:07 -0400

    write simplify_directions method

---
+++ exercises/simplify_directions.rb
@@ -0,0 +1,38 @@
+=begin
+["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+["WEST"]
+
+input: array
+output: array
+
+directions annihilate only if immediete next to each other
+NORTHSOUTH
+SOUTHNORTH
+EASTWEST
+WESTEAST
+
+if array is empty, return empty array
+if directions anihilate, return empty array
+
+turn array into string without commas or spaces
+match annihilating strings and replace with empty strings
+repeat until no more matches
+turn string into array
+
+1. join array into string without seperation
+2. times half the length of array, match with patterns and replace with empty string
+3. split based on matching NORTH SOUTH EAST WEST
+4. return array
+=end
+
+def simplify_directions(directions)
+	directions_string = directions.join
+	(directions.length / 2).times do
+		directions_string.gsub!(/(NORTHSOUTH)|(EASTWEST)|(SOUTHNORTH)|(WESTEAST)/, "")
+	end
+	 directions = directions_string.split(/(NORTH)|(SOUTH)|(EAST)|(WEST)/)
+	 directions.delete("")
+	 directions
+end
+
+p simplify_directions(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])
