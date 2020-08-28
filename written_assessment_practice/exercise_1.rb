b9e66a527254603a26385599be60505e8b15ac22
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-08-28 12:57:42 -0400

    rewrite written assessment solution

--- written_assessment_practice/exercise_1.rb
+++ written_assessment_practice/exercise_1.rb
@@ -22,4 +22,17 @@ When `arr` is re-assigned on line 3, it no longer references the same array as t
 Mutating changes called on `arr` no longer affect the array `greeting` references, since `arr` now references another array.
 Therefore, calling console.log on line 9 will print the array `["Hello"]` unchanged.
 
+==== TRY SOLUTION AGAIN ====
+
+** START WITH ANSWERING THE QUESTION **
+On line 9, `console.log` function call prints `["Hello"]` to the console because:
+  1) The global variable `greeting` is not reassigned in the body of the function `test`
+  2) The object that `greeting` points to is not mutated within the function 'test'.
+
+** THEN EXPLAIN IN DETAIL ONLY THE LINES THAT ARE RELEVANT TO ANSWER **
+On line 3, the `arr` parameter is reassigned to the new array `["ByeBye"]`.
+The parameter `arr` no longer references the same object as `greeting`.
+From now, we can't mutate the object the `greeting` variable points to, because `arr` references a different object.
+
+
 =end
