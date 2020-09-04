369e9235e02b236d1465e5cf0df1aabe04cddd56
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-09-04 10:00:02 -0400

    refactor format_date

--- regex/format_date.rb
+++ regex/format_date.rb
@@ -1,18 +1,7 @@
 def format_date(date)
-  if /([\-\/])\d\d\1/.match?(date)
-    date.gsub!(/[\-\/]/, ".")
-    date_parts = date.scan(/(\d\d\d\d)(\.)(\d\d)(\.)(\d\d)/)[0]
-    date_parts.reverse.join
-  else date end
+  date.sub(/(\d\d\d\d)([\-\/])(\d\d)\2(\d\d)/, '\4.\3.\1')
 end
 
-def format_date_original(date)
-  date.sub(/\A(\d\d\d\d)-(\d\d)-(\d\d)\z/, '\3.\2.\1')
-end
-
-p format_date_original('2016-06-17') # -> '17.06.2016'
-p format_date_original('2016/06/17') # -> '2016/06/17' (no change)
-
-# p format_date('2016-06-17') # -> '17.06.2016'
-# p format_date('2017/05/03') # -> '03.05.2017'
-# p format_date('2015/01-31') # -> '2015/01-31' (no change)
+p format_date('2016-06-17') # -> '17.06.2016'
+p format_date('2017/05/03') # -> '03.05.2017'
+p format_date('2015/01-31') # -> '2015/01-31' (no change)
