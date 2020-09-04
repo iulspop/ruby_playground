17720379ffe7dbcdb23c5cc81f167beafd9ff741
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-09-04 11:19:47 -0400

    refactor format_date

--- regex/format_date.rb
+++ regex/format_date.rb
@@ -1,5 +1,6 @@
-def format_date(date)
-  date.sub(/(\d\d\d\d)([\-\/])(\d\d)\2(\d\d)/, '\4.\3.\1')
+def format_date(orginal_date)
+  date_regex = /(?<year>\d\d\d\d)(?<seperator>[\-\/])(?<month>\d\d)\k<seperator>(?<day>\d\d)/
+  orginal_date.sub(date_regex, '\k<day>.\k<month>.\k<year>')
 end
 
 p format_date('2016-06-17') # -> '17.06.2016'
